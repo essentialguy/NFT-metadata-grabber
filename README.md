795ccc13d8a4eaf5c1cc0b3beebbf1a9aca5437d
Fazelpejmanfar <fazelpejmanfar@gmail.com>
2022-09-10 17:43:34 +0430

    create the magic

---
+++ README.md
@@ -0,0 +1,34 @@
+# NFT-metadata-grabber
+
+
+
+## Getting Started 
+
+a tool to grab nft metadata by passing ipfs cid of metadata
+
+
+### Installing
+
+```
+npm install
+```
+to run the grabber\
+the command take 3 args to run\
+startindex as first arg\
+endindex as second arg\
+CID as third arg
+ 
+```
+npm run grab 
+```
+
+### Example
+
+you need to pass startindex and endindex and Metadata CID as args\
+startindex = 1 \
+endindex = 5555 \
+CID = Qmd5DbGu4q6hMziNBqwyHEYZmBUsG4mTTR2DPypTu6TGvH
+
+```
+npm run grab 1 5555 Qmd5DbGu4q6hMziNBqwyHEYZmBUsG4mTTR2DPypTu6TGvH
+```
---
+++ index.js
@@ -0,0 +1,34 @@
+const fs = require("fs");
+const axios = require('axios').default;
+const args = process.argv.slice(2);
+const dir = `${__dirname}`;
+const JsonPath = `${dir}/jsons`;
+const startIndex = args[0];
+const endIndex = args[1];
+const CID = args[2];
+
+const getjson = () => {
+    fs.readdir(JsonPath, (err) => {
+        if (err) {
+        fs.mkdirSync(`${dir}/jsons`, { recursive: true});
+        console.log('Creating Path to Store Jsons...');
+      }
+      });
+    console.log(`%cFetching ${endIndex} json from ${CID}`, `color: red`);
+    fs.rmSync(JsonPath, { recursive: true, force: true });
+    fs.mkdirSync(`${dir}/jsons`, { recursive: true});
+    for(let i = startIndex; i <= endIndex; i++) {
+    axios.get(`https://sweetsea.mypinata.cloud/ipfs/${CID}/${i}.json`)
+  .then(function (response) {
+    fs.writeFileSync(`${JsonPath}/${i}.json`, JSON.stringify(response.data, null, 2));
+  })
+  .catch(function (error) {
+    console.log(error);
+  })
+}
+console.log(`%c${endIndex} json Fetched from ${CID}`, `color: red`);
+};
+
+
+getjson();
+
